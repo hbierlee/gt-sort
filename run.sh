dbf58f6884e01031d99ca8abf0be83c6133906c8
Hendrik Bierlee <hendrik.bierlee@monash.edu>
2024-05-31 14:43:51 +0200

    Rename and add results

--- run.sh
+++ run.sh
@@ -1,19 +1,20 @@
 #!/usr/bin/env sh
 
-seeds=10
+seeds=$1
 solver=chuffed
 
-n=12
-ub=10
+n=$2
+ub=$3
+f=$4
 
-echo "n=$n;ub=$ub"
+echo "n=$n;ub=$ub;f=$f"
 
 rm -f std.*.out
 for (( i = 1; i <= $seeds; i++ )); do
   echo "seed ${i}"
-  minizinc card.mzn -r $i --solver $solver --output-time -D "n=$n;ub=$ub;run=WORST" | tee >> std.$i.out
-  minizinc card.mzn -r $i --solver $solver --output-time -D "n=$n;ub=$ub;run=BASELINE" | tee >> std.$i.out
-  minizinc card.mzn -r $i --solver $solver --output-time -D "n=$n;ub=$ub;run=BEST" | tee >> std.$i.out
+  minizinc gt-sort.mzn -r $i --solver $solver --output-time -D "n=$n;ub=$ub;f=$f;run=WORST" | tee >> std.$i.out
+  minizinc gt-sort.mzn -r $i --solver $solver --output-time -D "n=$n;ub=$ub;f=$f;run=BASELINE" | tee >> std.$i.out
+  minizinc gt-sort.mzn -r $i --solver $solver --output-time -D "n=$n;ub=$ub;f=$f;run=BEST" | tee >> std.$i.out
   grep "obj =" std.$i.out
 done
 
