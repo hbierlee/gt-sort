0a06fc908f9309c5d5bfe17ed5bd327d631a9b8d
Hendrik Bierlee <hendrik.bierlee@monash.edu>
2024-05-31 15:56:55 +0200

    Support n's which are not powers of 2

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 seeds=$1
-solver=chuffed
+solver=chuffed # ! sometimes causes a bug with nosets.mzn for chuffed (I think if constraint is unsatisfiable)
 
 n=$2
 ub=$3
@@ -12,9 +12,10 @@ echo "n=$n;ub=$ub;f=$f"
 rm -f std.*.out
 for (( i = 1; i <= $seeds; i++ )); do
   echo "seed ${i}"
-  minizinc gt-sort.mzn -r $i --solver $solver --output-time -D "n=$n;ub=$ub;f=$f;run=WORST" | tee >> std.$i.out
-  minizinc gt-sort.mzn -r $i --solver $solver --output-time -D "n=$n;ub=$ub;f=$f;run=BASELINE" | tee >> std.$i.out
-  minizinc gt-sort.mzn -r $i --solver $solver --output-time -D "n=$n;ub=$ub;f=$f;run=BEST" | tee >> std.$i.out
+
+  for run in WORST BASELINE BEST; do
+    minizinc gt-sort.mzn -r $i --solver $solver --output-time -D "n=$n;ub=$ub;f=$f;run=$run" | tee >> std.$i.out
+  done
   grep "obj =" std.$i.out
 done
 
